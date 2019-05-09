890e169d6cae3a9a06092a9161a4066bc00409f3
William Moreno <williammorenoprieto@gmail.com>
2019-05-09 18:20:58 -0500

    DFS

--- src/graphVertices.java
+++ src/graphVertices.java
@@ -1,60 +1,112 @@
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Stack;
 
 public class graphVertices<T> {
 	private List<Vertice<T>> vertices;
-	
-	public  void addVertex(T dato) {
+	private boolean dirigido;
+
+	public graphVertices(boolean dirigido) {
+		super();
+		this.vertices = new ArrayList<Vertice<T>>();
+		this.dirigido = dirigido;
+	}
+
+	@Override
+	public String toString() {
+		return "graphVertices [vertices=" + vertices + ", dirigido=" + dirigido + "]";
+	}
+
+	public void addVertex(T dato) {
 		vertices.add(new Vertice<T>(dato));
 	}
-	public  void addEdge(T origin, T destination, Double weight) {
-		boolean estaori = hasVertex(origin);		
+
+	public void addEdge(T origin, T destination, Double weight) {
+		boolean estaori = hasVertex(origin);
 		boolean estades = hasVertex(destination);
-		
-		if(estades && estaori)
-		{
+
+		if (estades && estaori) {
 			for (Vertice<T> vertice : vertices) {
-				if(vertice.getDato().equals(origin))
-				{
+				if (vertice.getDato().equals(origin)) {
 					vertice.addEdgeVertex(origin, destination, weight);
 				}
 			}
-		}
-		else
-		{
-			System.out.println("ERROR NO EXISTE UN VERTICE");		
+			if (!dirigido) {
+				for (Vertice<T> vertice : vertices) {
+					if (vertice.getDato().equals(destination)) {
+						vertice.addEdgeVertex(destination, origin, weight);
+					}
+				}
+			}
+		} else {
+			System.out.println("ERROR NO EXISTE UN VERTICE");
 		}
 	}
-	public  boolean hasVertex( T vertex) {
+
+	public boolean hasVertex(T vertex) {
 		for (Vertice<T> vertice : vertices) {
-			if(vertice.getDato().equals(vertex))
-			{
+			if (vertice.getDato().equals(vertex)) {
 				return true;
 			}
 		}
 		return false;
 	}
-	
-	public  boolean hasEdge(T origin, T destination)
-	{
+
+	public boolean hasEdge(T origin, T destination) {
 		for (Vertice<T> vertice : vertices) {
-			if(vertice.getDato().equals(origin))
-			{
-				return vertice.hasEdge(origin,destination);				
+			if (vertice.getDato().equals(origin)) {
+				return vertice.hasEdge(origin, destination);
 			}
 		}
 		return false;
 	}
-	
+
+	public List<T> dfs() {
+		List<T> resultado = new ArrayList<T>();
+		Stack<T> pila = new Stack<T>();
+		vertices.get(0).setVisitado(true);
+		resultado.add(vertices.get(0).getDato());
+		for (Aristas arista : vertices.get(0).getVecinos()) {
+			pila.push((T) arista.getDestino());
+		}
+		while (!pila.isEmpty()) {
+			T top = pila.pop();
+			int pos = posDato(top);
+			if(!vertices.get(pos).isVisitado())
+			{
+				vertices.get(pos).setVisitado(true);
+				resultado.add(top);				
+				for (Aristas arista : vertices.get(pos).getVecinos() ) {
+					pila.push((T) arista.getDestino());
+				}
+			}
+		
+		}
+		return resultado;
+	}
+
+	private int posDato(T top) {
+		int pos=-1;
+		for (int i = 0; i < vertices.size(); i++) {
+			if (vertices.get(i).getDato().equals(top)) {
+				pos=i;
+				break;
+			}
+		}
+		return pos;
+	}
+
 	/*
-	--public  void addVertex(T vertex); 
-	--public  void addEdge(T origin, T destination, Double weight);
-	--public  boolean hasVertex( T vertex);
-	--public  boolean hasEdge(T origin, T destination);
-	
-	public  Vector<T> dfs (); //pila
-	public  Vector<T> bfs (); //cola
-	public  Vector< Pair<T, Double> > dijkstra (); // un nodo a muchos ~ cola de prioridad
-	public  Vector< Pair<T, Double> > floydWarshall (); // de todos a todos
-	public  Vector< T > kruskall (); //arbol de expansion minima - algoritmo voraz 
-	public  Vector< T > prim (); //arbol de expansion minima - programacion dinamica */
+	 * --public void addVertex(T vertex); 
+	 * --public void addEdge(T origin, T destination, Double weight); 
+	 * --public boolean hasVertex( T vertex); 
+	 * --public boolean hasEdge(T origin, T destination);
+	 * 
+	 * public Vector<T> dfs (); //pila public Vector<T> bfs (); //cola public
+	 * Vector< Pair<T, Double> > dijkstra (); // un nodo a muchos ~ cola de
+	 * prioridad public Vector< Pair<T, Double> > floydWarshall (); // de todos a
+	 * todos public Vector< T > kruskall (); //arbol de expansion minima - algoritmo
+	 * voraz public Vector< T > prim (); //arbol de expansion minima - programacion
+	 * dinamica
+	 */
 }
---
+++ src/main.java
@@ -0,0 +1,33 @@
+
+public class main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		graphVertices<Character> grafo = new graphVertices<Character>(false);
+		grafo.addVertex('a');
+		grafo.addVertex('b');
+		grafo.addVertex('c');
+		grafo.addVertex('d');
+		grafo.addVertex('e');
+		grafo.addVertex('f');
+		grafo.addVertex('g');
+		
+		grafo.addEdge('a', 'b', (double) 1);
+		grafo.addEdge('a', 'd', (double) 1);
+		grafo.addEdge('a', 'f', (double) 1);
+		grafo.addEdge('b', 'c', (double) 1);
+		grafo.addEdge('b', 'e', (double) 1);
+		grafo.addEdge('f', 'g', (double) 1);
+		
+		//System.out.println(grafo);
+		System.out.println(grafo.hasEdge('a', 'c'));
+		System.out.println(grafo.hasEdge('a', 'f'));
+		
+		System.out.println(grafo.hasVertex('z'));
+		System.out.println(grafo.hasVertex('f'));
+		
+		System.out.println(grafo.dfs());
+		
+	}
+
+}
